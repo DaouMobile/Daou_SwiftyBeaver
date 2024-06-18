f08f362cc5dc4c24028c98648a0af8bd5561467f
vongvoro <vongvoro@daou.co.kr>
2024-06-18 16:08:09 +0900

    add privacyInfo file

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.7.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,16 +6,16 @@ import PackageDescription
 let package = Package(
     name: "SwiftyBeaver",
     platforms: [
-        .macOS(.v10_10),
-        .iOS(.v8),
-        .tvOS(.v9),
-        .watchOS(.v2)
+        .macOS(.v11),
+        .iOS(.v12),
+        .tvOS(.v12),
+        .watchOS(.v4)
     ],
     products: [
         .library(name: "SwiftyBeaver", targets: ["SwiftyBeaver"])
     ],
     targets: [
-        .target(name: "SwiftyBeaver", path: "Sources"),
+        .target(name: "SwiftyBeaver", path: "Sources", resources: [.process("Resources/PrivacyInfo.xcprivacy")]),
         .testTarget(name: "SwiftyBeaverTests", dependencies: ["SwiftyBeaver"]),
     ],
     swiftLanguageVersions: [.v5]
